d69e0c77782f43fb5809f7ddee5607727907ebd7
Natnahom <natnahom12@gmail.com>
2022-09-08 21:34:19 +0300

    got it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int main(){
-
-    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-  
-  return 1;
+/**
+*main - prints with out puts and printf
+*
+*Description: using main function
+*Return: 0
+*/
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
