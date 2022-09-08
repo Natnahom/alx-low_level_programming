0eb3dcb8775740c89b4db9b3c166e9d90d466492
Natnahom <natnahom12@gmail.com>
2022-09-08 21:09:08 +0300

    got it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-
-int main(){
-
+/*
+*main - using variables
+*
+*Description: using main function
+*Return: 0
+*/
+int main(void){
     int a;
     long int b;
     long long int c;
     float d;
     char f;
-
     printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
     printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
     printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
     printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
     printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
-
 return 0;
 }
