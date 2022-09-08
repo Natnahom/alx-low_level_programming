f610e6f2cbeb945dc0c12d52cdb03e831a2f8081
Natnahom <natnahom12@gmail.com>
2022-09-08 21:05:23 +0300

    got it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 /**
 *main - print the string in the put function
 *
-*Description: using the main function
-*this program prints with proper grammer, but the outcome is a piece of art,
+*Description: using the main function,
 *Return: 0
 */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
