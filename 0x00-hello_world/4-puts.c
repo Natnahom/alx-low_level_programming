6f9d217c8b337883a2e806e38879ee6f6e26e271
Natnahom <natnahom12@gmail.com>
2022-09-08 20:20:28 +0300

    trying

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /*
-*main - Entry point
-*
-*Return: (0);
+*main - main block
+*Return: 0
 */
-int main(void){
-    
-    char str[] = "\"programming languages\n";
-
-    puts(str);
-    
-    return 0;
+int main(void)
+{
+puts("\"programming is like building a multilingual\n");
+return (0);
 }
