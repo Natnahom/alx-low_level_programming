d431fc6304c01ae81b6829695d9522fa70562122
Natnahom <natnahom12@gmail.com>
2022-09-13 16:16:55 +0300

    functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,12 +27,10 @@ _putchar((res % 10) + '0');
 else if (res < 10 && j != 0)
 {
 _putchar(' ');
-_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
 }
 else
-{
-_putchar((res / 10) + '0');
-}
+_putchar((res % 10) + '0');
 }
 _putchar('\n');
 }
