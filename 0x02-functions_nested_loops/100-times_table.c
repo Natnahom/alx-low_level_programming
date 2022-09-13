ec369c923faebb87f49f5b069d94feb4a82cfbcf
Natnahom <natnahom12@gmail.com>
2022-09-13 18:00:53 +0300

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,18 +2,19 @@
 /**
 *print_times_table - prints n times table
 *@n: input
-*Return: n times
+*Return: nothing
 */
 void print_times_table(int n)
 {
 int i, j, res;
-	if (!(n > 15 || n < 0))
-	{
+
+if (!(n > 15 || n < 0))
+{
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-		res = i * j;
+		res = (i * j);
 		if (j != 0)
 		{
 			_putchar(',');
@@ -38,9 +39,9 @@ int i, j, res;
 			_putchar((res % 10) + '0');
 		}
 		else
-			_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
 		}
 	_putchar('\n');
 	}
-	}
+}
 }
