f90277cf8c0cb018d95f0ae798977a8f6129764e
Natnahom <natnahom12@gmail.com>
2022-09-13 20:18:40 +0300

    functions

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints
+*
+* Description: using main function
+* Return: 0
+*/
+int main(void)
+{
+	unsigned long count, i, j, k, sums;
+i = sums = 0;
+j = 1;
+
+	for (count = 0; count < 98; count++)
+	{
+	k = i + j;
+	i = j;
+	j = k;
+	printf("%lu", k);
+	printf(", ");
+	}
+	printf("\n", k);
+return (0);
+}
