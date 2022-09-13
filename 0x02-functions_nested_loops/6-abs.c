95cc75a615dc5dbfaa98961fffa48c328c215d11
Natnahom <natnahom12@gmail.com>
2022-09-13 15:20:54 +0300

    functions

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
 *_abs - converts it to positive value
-*@i: character to test
+*@n: character to test
 *Return: 1 or 0
 */
-int _abs(int i)
+int _abs(int n)
 {
-if (i < 0)
-{
-i = i * -1;
-return (i);
-}
+if (n < 0)
+n = -n;
+return (n);
 }
