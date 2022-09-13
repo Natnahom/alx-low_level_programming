7a83b398098fcb008a192c5bd4fd3bde4bb667c0
Natnahom <natnahom12@gmail.com>
2022-09-13 16:51:15 +0300

    functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_to_98 - prints natural numbers from n-98
+*@n: initial number
+*Return: nothing
+*/
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	return (n);
+}
