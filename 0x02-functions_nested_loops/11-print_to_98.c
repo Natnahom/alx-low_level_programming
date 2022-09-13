da3e01cd023b957cce7d3bba46935506ca455751
Natnahom <natnahom12@gmail.com>
2022-09-13 17:00:45 +0300

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,6 +6,16 @@
 */
 void print_to_98(int n)
 {
-	for (n = n; n <= 98; n++)
-	return (n);
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		printf("%d, ", n);
+		printf("%\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		printf("%d, ", n);
+		printf("%d\n", 98);
+	}
 }
