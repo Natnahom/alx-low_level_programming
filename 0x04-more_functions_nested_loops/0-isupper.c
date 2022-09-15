740ae7a8f2142c8a4bde7985d8e2807e16162aeb
Natnahom <natnahom12@gmail.com>
2022-09-15 09:34:16 +0300

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isupper - prints if c is upercase or not
+*@c: character
+*Return: 1 if upercase else 0
+*/
+int _isupper(int c)
+{
+int c;
+
+	if (c == 'A')
+	return (1);
+	else
+	return (0);
+}
