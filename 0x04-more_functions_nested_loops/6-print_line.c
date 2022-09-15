a2130380e8407452fba8cacf8e892acd1d78f29b
Natnahom <natnahom12@gmail.com>
2022-09-15 13:19:36 +0300

    functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
 *print_line - prints line
 *@n: number of line
-*Return: nothing
 */
 void print_line(int n)
 {
 int x;
 
-	if (n <= 0)	
+	if (n <= 0)
 		_putchar('\n');
 	for (x = 0; x <= n; x++)
 		_putchar('_');
