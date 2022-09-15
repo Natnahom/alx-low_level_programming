2e84c204f5f4c74ab393d4f9e582148200fc050d
Natnahom <natnahom12@gmail.com>
2022-09-15 12:49:10 +0300

    functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,12 @@
  */
 void print_most_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+int i;
+
+	for (i = 0; i < 10; i++)
 	{
-	if (i == 2 || i == 4)
-	{
-	continue;
-	}
-	else
-	_putchar(i);
+	if (i != 2 && i != 4)
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
