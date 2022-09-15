c7317fd12fb65c57427e33796bf9edd534412b14
Natnahom <natnahom12@gmail.com>
2022-09-15 18:04:44 +0300

    functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the largest prime factor of 612852475143
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	long long i;
+	long long number = 612852475143;
+
+		for (i = 2; i < number; i++){
+		if (number % i == 0)
+		number /= i;
+	}
+		printf("%li\n", number);
+	return 0;
+}
