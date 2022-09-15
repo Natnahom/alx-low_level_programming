eceeecd50167dfe64454fe229521ae0250b2b9d9
Natnahom <natnahom12@gmail.com>
2022-09-15 10:56:52 +0300

    functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,7 @@
 */
 int _isupper(int c)
 {
-int c;
-
-	if (c == 'A')
+	if (c >= 'A' && c <= 'Z')
 	return (1);
 	else
 	return (0);
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_numbers - prints from 0-9
+*Return: nothing
+*/
+void print_numbers(void)
+{
+	for (int i = '0'; i <= '9'; i++)
+	_putchar(i);
+	_putchar('\n');
+}
