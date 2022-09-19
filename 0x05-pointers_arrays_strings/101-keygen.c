ddc5b9fb28b2a419413d0c73ec6b4e6507cb67a0
Natnahom <natnahom12@gmail.com>
2022-09-19 20:44:33 +0300

    final task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+	i = (rand() % 9999 + 1);
+
+	printf("Checking code: %d\n", i);
+
+	return (0);
+}
