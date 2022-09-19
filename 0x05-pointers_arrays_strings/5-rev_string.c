953cc1ff869b5718e4974c84bba4331e41768ce6
Natnahom <natnahom12@gmail.com>
2022-09-19 16:00:06 +0300

    strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,26 @@
 #include "main.h"
-#include <string.h>
 /**
  * rev_string - reverses a string
  * @s: string
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	char temp;
+	int i, n1, n2;
+
+	n1 = 0;
+	n2 = 0;
+
+	while (s[n1] != '\0')
+	{
+		n1++;
+	}
+	n2 = n1 - 1;
+	for (i = 0; i < n2; i++)
+	{
+		temp = s[i];
+		s[i] = s[n2];
+		s[n2] = temp;
+		n2 -= 1;
+	}
 }
