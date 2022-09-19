e9dfc132865023f5635719b54cca858debf9e142
Natnahom <natnahom12@gmail.com>
2022-09-19 21:06:11 +0300

    final task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,11 +9,17 @@
 int main(void)
 {
 	int i;
+	int random;
+	int total;
 
 	srand(time(NULL));
-	i = (rand() % 9999 + 1);
+	for (i = 0, total = 2772; total > 122; i++)
+	{
+		random = (rand() % 125) + 1;
+		printf("%c", random);
+		total -= random;
+	}
 
-	printf("%c", i);
-
+	printf("%c", total);
 	return (0);
 }
