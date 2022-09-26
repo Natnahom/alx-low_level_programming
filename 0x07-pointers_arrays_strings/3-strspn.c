65350e20fac4d8a4fa5349756885c720c8759d9f
Natnahom <natnahom12@gmail.com>
2022-09-26 11:45:44 +0300

    strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string 1
+ * @accept: string 2
+ * Return: length of substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		if (*s == *accept)
+		{
+			return (i + 1);
+		}
+		s++;
+		i++;
+	}
+	return (i + 1);
+}
