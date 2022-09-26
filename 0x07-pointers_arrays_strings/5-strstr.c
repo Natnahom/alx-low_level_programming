ee6dfece5c8ad64fd2cf61df0daf76bfd86079e3
Natnahom <natnahom12@gmail.com>
2022-09-26 13:09:46 +0300

    strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string 1
+ * @needle: string 2
+ * Return: needle or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *s1, *s2;
+
+	while (*haystack != '\0')
+	{
+		s1 = haystack;
+		s2 = needle;
+
+		while (*haystack != '\0' && *s2 != '\0' && *haystack == *s2)
+		{
+			haystack++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return (s1);
+		haystack = s1 + 1;
+	}
+
+	return (0);
+}
