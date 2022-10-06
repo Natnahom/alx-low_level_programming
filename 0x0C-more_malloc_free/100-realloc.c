78612289cd96c11d65a39f320acd49cd6f53d8f4
Natnahom <natnahom12@gmail.com>
2022-10-06 10:13:22 +0300

    allocation

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a memory
+ * @ptr: pointer
+ * @old_size - integer
+ * @new_size - integer
+ * Return: p or Null
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	int *p;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free (ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc (new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+	if ((new_size > old_size) && (ptr != NULL))
+	{
+		p = malloc (new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			p[i] = *((char *)ptr + 1);
+		free(ptr);
+	}
+
+	return (p);
+}
