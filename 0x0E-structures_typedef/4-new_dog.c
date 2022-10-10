8e751dd884a1980f7169f01fd31e2fe64d1dcec1
Natnahom <natnahom12@gmail.com>
2022-10-10 14:33:02 +0300

    structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create new dog
+ * @name: character
+ * @age: float
+ * @owner: character
+ * Return: doggy
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int new, old, i;
+	dog_t *doggy;
+
+	new = old = 0;
+	while (name[new++])
+		;
+	while (owner[old++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+
+	doggy->name = malloc(new * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < new; i++)
+		doggy->name[i] = name[i];
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
+}
