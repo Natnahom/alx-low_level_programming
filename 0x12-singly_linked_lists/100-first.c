2599dd86564871b577a3257ebbf140dd1ed2e689
Natnahom <natnahom12@gmail.com>
2022-10-20 17:28:32 +0300

    lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print - prints string before the main function
+ */
+void print(void)__attribute__((constructor));
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
