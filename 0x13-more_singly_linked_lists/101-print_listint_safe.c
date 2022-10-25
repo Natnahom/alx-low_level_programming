2d036e7eee8aaebf3bf467b4f2d5ca743efcb868
Natnahom <natnahom12@gmail.com>
2022-10-25 15:10:04 +0300

    fixed

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+  * print_listint_safe - define function
+  * @head: lists
+  * Return: what does it return?
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
+
+	current = head;
+	if (current == NULL)
+		exit(98);
+
+	count = 0;
+	while (current != NULL)
+	{
+		hold = current;
+		current = current->next;
+		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
+
+		if (hold < current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+
+	return (count);
+}
