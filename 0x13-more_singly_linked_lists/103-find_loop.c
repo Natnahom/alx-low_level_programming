8a6f445be70eee3825b78d9dfc9ddb143fee1b90
Natnahom <natnahom12@gmail.com>
2022-10-25 15:14:38 +0300

    fixed

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,7 +3,7 @@
 /**
  * find_listint_loop - find the loop in a linked list
  * @head: head of linked list
- * 
+ *
  * Return: Address of node where loop starts, or NULL if no loop found.
  */
 listint_t *find_listint_loop(listint_t *head)
