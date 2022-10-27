07e44c8463ff0cf8f55e680678197cc7f60766ab
Natnahom <natnahom12@gmail.com>
2022-10-27 21:54:54 +0300

    bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * get_endianness - returns processor endianness
+  * Return: 0 if big endian, 1 if little endian
+  */
+int get_endianness(void)
+{
+	int i;
+	char *p;
+
+	i = 1;
+	p = (char *)&i;
+	return ((int)p[0]);
+}
